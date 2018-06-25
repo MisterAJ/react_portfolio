ec753db2f4d322f718003cda2e5b6866f225f830
AJ Longstreet <ajlongstreet@AJs-MacBook-Pro.local>
2018-06-24 20:56:50 -0400

    adds footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Jumbotron from "./Components/jumbotron";
 import Skills from "./Components/skills";
+import Footer from "./Components/footer";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Jumbotron />
         <Skills />
+        <Footer />
       </div>
     );
   }
