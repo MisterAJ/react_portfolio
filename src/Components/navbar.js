d23cf5cf0de4e7e8bafa388f5d03555d7fbedbe8
AJ Longstreet <ajlongstreet@AJs-MacBook-Pro.local>
2018-06-26 23:20:45 -0400

    adds navbar component

---
+++ src/Components/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const NavContainer = styled.div`
+  width: 100%;
+`;
+
+class NavBar extends Component {
+  render() {
+    return <NavContainer />;
+  }
+}
+
+export default NavBar;
